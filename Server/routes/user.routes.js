b6f223117f3402f2327f0608b2dd383a0f46af6b
Rati Krishna Moharana <mratikrishna@gmail.com>
2025-11-25 23:00:34 +0530

    api done

---
+++ Server/routes/user.routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getUserData } from '../controllers/user.controller';
+import userAuth from '../middleware/userAuthMiddleware.js'
+
+const userRouter = express.Router();
+
+userRouter.get('/data', userAuth, getUserData);
+
+export default userRouter;
--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js'
 import authRouter from './routes/auth.routes.js'
+import userRouter from './routes/user.routes.js'
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -14,6 +15,7 @@ app.use(cookieParser())
 
 app.get('/', (req, res) => res.send('API Working!'));
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 app.listen(port, () => {
     console.log(`Server is running in ${port}`)
