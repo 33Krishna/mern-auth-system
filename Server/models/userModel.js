c7e6f3df53da8aa046965391a2d5efc0e51d9527
Rati Krishna Moharana <mratikrishna@gmail.com>
2025-11-30 15:30:16 +0530

    google auth added in api

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-    name: { type:String, required: true },
+    name: { type:String },
     email: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
+    password: { type: String },
+    googleId: { type: String },
+    avatar: { type: String },
     verifyOtp: { type: String, default: ''},
     verifyOtpExpireAt: { type: Number, default: 0 },
     isAccountVerified: { type: Boolean, default: false },
--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { isAuthenticated, login, logout, register, resetPassword, sendResetOtp, sendVerifyOtp, verifyEmail } from '../controllers/auth.controller.js';
 import userAuth from '../middleware/userAuthMiddleware.js';
+import passport from 'passport';
+import jwt from 'jsonwebtoken'
 
 const authRouter = express.Router();
 
@@ -13,5 +15,25 @@ authRouter.get('/is-auth', userAuth, isAuthenticated)
 authRouter.post('/send-reset-otp', sendResetOtp)
 authRouter.post('/reset-password', resetPassword)
 
+// Step-1: Redirect to Google Login
+authRouter.get('/google', passport.authenticate('google', { scope:['profile', 'email']}))
+
+authRouter.get('/google/callback', 
+    passport.authenticate('google', { session: false }),
+    (req, res) => {
+        try {
+            const token = jwt.sign({ id: req.user._id, email: req.user.email }, process.env.SECRET_KEY, { expiresIn: '7d'})
+            
+            res.redirect(`${process.env.CLIENT_URL}/auth-success?token=${token}`)
+        } catch (error) {
+            console.log('Google login error:', error)
+            res.redirect(`${process.env.CLIENT_URL}/login?error=google_failed`)
+        }
+    }
+)
+
+authRouter.get('/me', userAuth, (req, res) => {
+    res.json({ success: true, user: req.user})
+})
 
 export default authRouter;
